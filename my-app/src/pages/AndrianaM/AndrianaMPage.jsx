ad220eabb93998ebc012ea06f252b97f70587ca4
mykolaievych <72654463+mykolaievych@users.noreply.github.com>
2022-04-15 17:51:52 +0300

    add AndrianaMPage

---
+++ my-app/src/pages/AndrianaM/AndrianaMPage.jsx
@@ -0,0 +1,17 @@
+import s from "./AndrianaMPage.module.css"
+
+const AndrianaMPage = () => {
+    return (
+        <div className={s.container}>
+            <h3>top rezume</h3>
+            <img src="" alt="moya fotka"/>
+            <ul>
+                <li><h1>Andriana Mykolaievych</h1></li>
+                <li>age: 15</li>
+                <li>education: Zhovtanets'kyy opornyy ZZSO</li>
+            </ul>
+        </div>
+    )
+}
+
+export default AndrianaMPage;
