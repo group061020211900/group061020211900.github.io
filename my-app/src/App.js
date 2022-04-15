aa6bf253e4d646f4f40fb5f2fbe4f35120105c89
mykolaievych <72654463+mykolaievych@users.noreply.github.com>
2022-04-15 17:47:46 +0300

    add AndrianaMPage

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Navigate, Route, Routes, useLocation } from "react-router-dom";
 import IhorPage from "./pages/Igor/IhorPage";
+import AndrianaMPage from './pages/AndrianaM/AndrianaMPage'
 import NotFoundPage from "./pages/404/404";
 import { RouteConst } from "./common/RouteConst";
 import { showNavbar } from "./utils";
@@ -12,16 +13,17 @@ const App = () => {
     localStorage.setItem("isAdmin", true);
   return (
     <div className="App">
-        {showNavbar(path) && <h1>HEADER</h1>}
+        {/* {showNavbar(path) && <h1>HEADER</h1>} */}
         {/*//Todo create Link to your page*/}
         {/*{ !showNavbar ? <h2>False</h2> : <h3>True</h3>}*/}
         {/*<BrowserRouter>*/}
             <Routes>
-                <Route path={RouteConst.MENTOR} element={<IhorPage />}/>
-                {/*<Route path="igor" element={<IhorPage />}/>*/}
+                {/* <Route path={RouteConst.MENTOR} element={<IhorPage />}/> */}
+                <Route path="igor" element={<IhorPage />}/>
                 <Route path="teams" element={<IhorPage />}>
                     <Route path={RouteConst.USER_ID} element={<IhorPage />}/>
                 </Route>
+                <Route path="anjoanka" element={<AndrianaMPage />} />
                 <Route path={RouteConst.NOT_FOUND_PAGE} element={<NotFoundPage />} />
                 <Route
                     path="*"
