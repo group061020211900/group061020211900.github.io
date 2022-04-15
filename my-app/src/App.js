f2376e127e291fbb6fe321c42fadec3384025da7
ihor.kurylov@clikk.app <idSNrY7I3xhB>
2022-04-15 20:44:09 +0300

    lesson private route issues

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,28 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Navigate, Route, Routes, useLocation } from "react-router-dom";
+import { BrowserRouter, Link, Navigate, Route, Routes, useLocation } from "react-router-dom";
 import IhorPage from "./pages/Igor/IhorPage";
 import NotFoundPage from "./pages/404/404";
 import { RouteConst } from "./common/RouteConst";
 import { showNavbar } from "./utils";
+import AdminPage from "./pages/AdminPage/AdminPage";
+import { useEffect } from "react";
+import PrivateRoute from "./common/HOC/PrivateRoute";
 
 const App = () => {
     const path = useLocation().pathname;
     // console.log(path);
-    localStorage.setItem("isAdmin", true);
+
+    // useEffect(()=>{
+    //     localStorage.setItem("isAdmin", false);
+    // },[])
+
   return (
     <div className="App">
-        {showNavbar(path) && <h1>HEADER</h1>}
+        {showNavbar(path) && (<div>
+            <h1>HEADER</h1>
+            <Link to={RouteConst.IhorKurylov}>to mentors page</Link>
+        </div>)}
         {/*//Todo create Link to your page*/}
         {/*{ !showNavbar ? <h2>False</h2> : <h3>True</h3>}*/}
         {/*<BrowserRouter>*/}
             <Routes>
                 <Route path={RouteConst.MENTOR} element={<IhorPage />}/>
-                {/*<Route path="igor" element={<IhorPage />}/>*/}
+                {/*<Route element={<PrivateRoute />}>*/}
+                {/*    <Route path={RouteConst.ADMIN} element={<AdminPage />}/>*/}
+                {/*</Route>*/}
+                {/*<Route*/}
+                {/*    path={RouteConst.ADMIN}*/}
+                {/*    element={<PrivateRoute Component={AdminPage} />}*/}
+                {/*/>*/}
+
+                <Route path={RouteConst.IhorKurylov} element={<AdminPage />} />
+                <Route path={`${RouteConst.IhorKurylov}/form`} element={<NotFoundPage />} />
+
+                <Route path={RouteConst.IhorKurylov} element={<AdminPage />}>
+                    <Route path="404" element={<NotFoundPage />} />
+                </Route>
+
+
                 <Route path="teams" element={<IhorPage />}>
                     <Route path={RouteConst.USER_ID} element={<IhorPage />}/>
                 </Route>
-                <Route path={RouteConst.NOT_FOUND_PAGE} element={<NotFoundPage />} />
+                {/*<Route path={RouteConst.NOT_FOUND_PAGE} element={<NotFoundPage />} />*/}
                 <Route
                     path="*"
                     element={<Navigate to={RouteConst.NOT_FOUND_PAGE} />}
--- my-app/src/common/RouteConst.js
+++ my-app/src/common/RouteConst.js
@@ -2,6 +2,8 @@ export const RouteConst={
     NOT_FOUND_PAGE: "/error",
     USER_ID: ":userId",
     MENTOR: "/",
+    ADMIN: "/admin",
+    IhorKurylov: "/IhorKurylov"
 }
 
 export const IhorRoutes= {
