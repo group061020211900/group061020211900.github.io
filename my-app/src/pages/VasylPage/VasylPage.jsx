0c80987a7774fb6cf84de624a9ac0b8f22c854d7
Vasyl <mr.yatsishin@gmail.com>
2022-04-24 23:41:55 +0300

    fetch

--- my-app/src/pages/VasylPage/VasylPage.jsx
+++ my-app/src/pages/VasylPage/VasylPage.jsx
@@ -2,39 +2,48 @@ import { useState } from "react";
 import styles from "./VasylPage.module.css";
 import { useNavigate } from "react-router-dom";
 import { JSONPlaceholder } from "../../api/api";
-
 const VasylPage = () => {
+  var arr = [];
   const ToHome = useNavigate();
+  const [com, setCom] = useState(arr);
+  const [showComp, setShow] = useState(false);
   const [info, setInfo] = useState({
     name: "Василь Яцишин",
     age: 15,
   });
-  const [com, setCom] = useState();
-
-  const getData = () => {
+  const GetData = () => {
     JSONPlaceholder.getUsers()
       .then((response) => {
         var data = response.data;
-        for (let i = 0; i < response.data.length; i++) {
-            console.log("Name:" + data[i].name);
-            console.log("Id:" + data[i].id);
-        }
         const comment = data.map((el) => {
-          setCom(el.name);
+          arr.push(el);
         });
+        setShow(true);
+        setCom(arr);
       })
       .catch((e) => {
         e.response.status !== 200 &&
           console.log(`Error number ${e.response.status}`);
       });
   };
+
   return (
     <div className={styles.VasylPage}>
       <img src={info.photo} className={styles.photo}></img>
       <h1>Ім'я: {info.name}</h1>
       <h2>Вік: {info.age}</h2>
-      <h2>{com}</h2>
-      <button onClick={getData}>Get data from host</button>
+      <button
+        onClick={() => {
+          GetData();
+        }}
+      >
+        Get data from host
+      </button>
+      <div>
+        {showComp === true
+          ? com.map((el) => <h1 key={Math.random()}>{el.name}</h1>)
+          : null}
+      </div>
       <button onClick={() => ToHome("/")}>To home</button>
     </div>
   );
