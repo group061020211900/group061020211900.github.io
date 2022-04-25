55d8fbf472c830a7f70f0038acc5a1f5b48333d8
Vasyl <mr.yatsishin@gmail.com>
2022-04-25 13:53:36 +0300

    resolving

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import AndrianaMPage from './pages/AndrianaM/AndrianaMPage'
 import { BrowserRouter, Link, Navigate, Route, Routes, useLocation } from "react-router-dom";
 import NotFoundPage from "./pages/404/404";
 import { RouteConst } from "./common/RouteConst";
@@ -17,7 +18,6 @@ import IhorKurylovUsersPage from "./pages/AdminPage/Users/IhorKurylovUsersPage";
 const App = () => {
     const path = useLocation().pathname;
     // console.log(path);
-
     useEffect(()=>{
         localStorage.setItem("user", JSON.stringify({
             role: "admin"
@@ -53,6 +53,8 @@ const App = () => {
                 <Route path={RouteConst.NOT_FOUND_PAGE} element={<NotFoundPage />} />
                 <Route path={RouteConst.DANYLO} element={<DanyloPage />} />
                 <Route path={RouteConst.VASYLYATSISHIN} element={<VasylPage />} />
+                <Route path={RouteConst.VASYLYATSISHIN} element={<VasylPage/>} />
+                <Route path={RouteConst.ANDRIANA_M} element={<AndrianaMPage />} />
                 <Route
                     path="*"
                     element={<Navigate to={RouteConst.NOT_FOUND_PAGE} />}
--- my-app/src/pages/Igor/IhorPage.jsx
+++ my-app/src/pages/Igor/IhorPage.jsx
@@ -1,51 +1,65 @@
-import styles from "./IhorPage.module.css"
+import styles from "./IhorPage.module.css";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { RouteConst } from "../../common/RouteConst";
 import { useEffect, useState } from "react";
 
+const Counter = ({ decrease, increase, counter }) => {
+    console.log('Counter')
+  return (
+    <div>
+      <button onClick={decrease}> plus</button>
+      <br />
+      <br />
+      <br />
+      <button onClick={increase}> minus</button>
+      {/*<p>Counter {counter}</p>*/}
+    </div>
+  );
+};
+
+const AnotherCounter = ({ divide, multiple, counter }) => {
+  return (
+    <div>
+      <button onClick={divide}> divide</button>
+      <br />
+      <br />
+      <button onClick={multiple}> multiple</button>
+      <p>Counter {counter}</p>
+    </div>
+  );
+};
+
 const MainPage = () => {
-    const navigate = useNavigate();
-    // const [count, setCount] = useState(1)
-    // const [aCount, setACount] = useState(10)
+  const navigate = useNavigate();
+  const [counter, setCounter] = useState(5);
 
-    // useEffect(()=>{
-    //     console.log("empty useEffect", count)
-    // })
-    // useEffect(()=>{
-    //     console.log("useEffect []", count)
-    // }, [])
-    //
-    // useEffect(()=>{
-    //     console.log("useEffect [aCount]", aCount)
-    // }, [aCount])
-    // useEffect(()=>{
-    //     console.log("useEffect [count]", count)
-    // }, [count])
-    //
-    // useEffect(() => {
-    //     const isAdmin  = localStorage.getItem("isAdmin");
-    //     console.log("isAdmin",isAdmin)
-    //     // isAdmin === true && navigate(RouteConst.NOT_FOUND_PAGE)
-    // },[])
+  const decrease = () => {
+    setCounter((prevState) => prevState + 1);
+  };
+  const increase = () => {
+    setCounter((prevState) => prevState - 1);
+  };
+  const divide = () => {
+    setCounter((prevState) => prevState / 2);
+  };
+  const multiple = () => {
+    setCounter((prevState) => prevState * 2);
+  };
 
-    // useEffect(() => {
-    //     const isAdmin  = localStorage.getItem("isAdmin");
-    //     console.log("isAdmin",isAdmin)
-    //     isAdmin === true && navigate(RouteConst.NOT_FOUND_PAGE)
-    // },[])
-
-    return (
-        <div className={styles.container}>
-            <h1>Main page</h1>
-            <Link to={RouteConst.IhorKurylov}>Go to Mentors page</Link>
-            <br/>
-            <Link to={RouteConst.MARK}>Go to {RouteConst.MARK} page</Link>
-            {/*//TODO set redirects to your Route*/}
-            {/*<h1 onClick={()=> navigate(RouteConst.NOT_FOUND_PAGE)}>IhorPage</h1>*/}
-            {/*<h1 onClick={()=> setCount((prevState) => ++prevState )}>IhorPage count {count}</h1>*/}
-            {/*<h1 onClick={()=> setACount((prevState) => ++prevState )}>IhorPage Another aCount {aCount}</h1>*/}
-        </div>
-    )
-}
+  return (
+    <div className={styles.container}>
+      <h1>Main page</h1>
+      <Counter decrease={decrease} increase={increase} counter={counter} />
+      <AnotherCounter divide={divide} multiple={multiple} counter={counter} />
+      <Link to={RouteConst.IhorKurylov}>Go to Mentors page</Link>
+      <br />
+      <Link to={RouteConst.MARK}>Go to {RouteConst.MARK} page</Link>
+      {/*//TODO set redirects to your Route*/}
+      {/*<h1 onClick={()=> navigate(RouteConst.NOT_FOUND_PAGE)}>IhorPage</h1>*/}
+      {/*<h1 onClick={()=> setCount((prevState) => ++prevState )}>IhorPage count {count}</h1>*/}
+      {/*<h1 onClick={()=> setACount((prevState) => ++prevState )}>IhorPage Another aCount {aCount}</h1>*/}
+    </div>
+  );
+};
 
 export default MainPage;
