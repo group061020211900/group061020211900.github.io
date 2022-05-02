b81b5f48001e47a5178e692f1a2247b008f37be9
kseroks <vitrikushihor0101@gmail.com>
2022-05-02 11:29:13 +0300

    Додав старе дз з кнопками яке лежало в гітхаб репозиторії

---
+++ my-app/src/pages/VitrikushIhorPage/ButtonDz/ContainerButtonVitrikush.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import { Button } from "./Button";
+import { RoutVitrikushIhorHome } from "../../../common/UsersRout/RoutVitrikushIhorHome";
+import s from "./Button.module.scss";
+
+export const ContainerButtonVitrikush = () => {
+  const colors = [
+    "red",
+    "green",
+    "blue",
+    "yellow",
+    "orange",
+    "purple",
+    "brown",
+    "black",
+    "white",
+  ];
+  const randomColor = () => colors[Math.floor(Math.random() * colors.length)];
+
+  const [color, setColor] = useState(randomColor);
+  const [colorBg, setColorBg] = useState(randomColor);
+
+  return (
+    <>
+      <div className={s.wrapperBtn}>
+        <Button
+          id={1}
+          setColor={setColor}
+          color={color}
+          setColorBg={setColorBg}
+          colorBg={colorBg}
+          randomColor={randomColor}
+        />
+        <Button
+          id={2}
+          setColorBg={setColorBg}
+          colorBg={colorBg}
+          setColor={setColor}
+          color={color}
+          randomColor={randomColor}
+        />
+      </div>
+      <div>
+        <RoutVitrikushIhorHome />
+      </div>
+    </>
+  );
+};
