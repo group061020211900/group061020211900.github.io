95a8df40806102ddf1851a22a6ab48182fb44a5e
kseroks <vitrikushihor0101@gmail.com>
2022-05-07 14:30:59 +0300

    fix vitrikushIhor types

--- my-app/src/redux/store.ts
+++ my-app/src/redux/store.ts
@@ -6,7 +6,6 @@ import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
 
 const rootReducer = combineReducers({
   postsReducer: postsReducer,
-  // @ts-ignore
   VitrikushReducer,
 })
 
